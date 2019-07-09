7d98b8aed6c5bfc709ec8dfbc990d962feb45aee
Daniel Franklyn Snow <dfsnow@la2.rcc.local>
2019-07-08 23:42:01 -0500

    Check if remaining file exists

--- submit_jobs_simple.sh
+++ submit_jobs_simple.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 docker run --rm \
-    -v /home/$USER/resources/graphs/:/resources/graphs/ \
-    -v /home/$USER/resources/outputs/:/resources/outputs/ \
-    -e TRAVEL_MODE='CAR' \
+    -v ~/resources/graphs/:/resources/graphs/ \
+    -v ~/resources/outputs/:/resources/outputs/ \
+    -e TRAVEL_MODE='TRANSIT,WALK' \
     -e TYPE='TRACT' \
     -e OVERWRITE_GRAPH='TRUE' \
-    -e MAX_TRAVEL_TIME=3600 \
+    -e MAX_TRAVEL_TIME=7200 \
     -e MAX_WALK_DIST=3000 \
     -e CHUNKS=100 \
-    -e MAX_THREADS=12 \
-    -e GEOID=17031 \
+    -e MAX_THREADS=4 \
+    -e GEOID=48029 \
     snowdfs/otp-routing
 
